14c5cd7834ce36f454222795333d0b15871ac9ac
Lucas Oliveira <blesslucas89@gmail.com>
2026-08-03 16:02:55 -0500

    KAN-23 Título: Criar endpoint de cadastro de usuário

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from src.database import TORTOISE_ORM
+from routers.public_router import router as public_router
+from tortoise.contrib.fastapi import register_tortoise
 
 app = FastAPI(title="Identity Service", version="1.0.0")
+
+register_tortoise(
+    app,
+    config=TORTOISE_ORM,
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
+app.include_router(public_router)
 
 
 @app.get("/")
 def root():
-    return {"message": "Identity Service running"}
+    return {"message": "Welcome to the Identity Service!"}
---
+++ src/routers/public_router.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Depends, status
+from src.schemas.user import UserCreate
+from src.services.user_service import UserService
+
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserCreate, service: UserService = Depends()):
+    return await service.create_user(user)

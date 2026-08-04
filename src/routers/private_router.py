391b95d3d1f57bdb38351b3655a4cfce7225b44c
Lucas Oliveira <blesslucas89@gmail.com>
2026-08-04 16:29:26 -0500

    KAN-51-KAN-52-KAN-55-KAN-60-KAN-65-KAN-74- Implementar hash de Senha concluido

--- src/routers/private_router.py
+++ src/routers/private_router.py
@@ -9,9 +9,7 @@ router = APIRouter(
 )
 
 
-router.get("/", response_model=list[UserGet], status_code=status.HTTP_200_OK)
-
-
+@router.get("/", response_model=list[UserGet], status_code=status.HTTP_200_OK)
 async def get_user():
     service = UserService()
 

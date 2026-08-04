25e677c18eeedfa099cdc3850425cd89e023f72f
Lucas Oliveira <blesslucas89@gmail.com>
2026-08-04 11:00:44 -0500

    KAN-25- Título: atualizacao de usuario feita-lembrar de implementar o jwt depois

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,5 +1,6 @@
 from src.models.user import User
-from src.schemas.user import UserCreate
+from src.schemas.user import UserCreate, UserUpdate
+from fastapi import HTTPException, status
 
 
 class UserService:
@@ -19,3 +20,19 @@ class UserService:
 
     async def get_user_by_id(self, user_id: int):
         return await User.filter(id=user_id).first()
+
+    async def put_user_by_id(self, user_id: int, user: UserUpdate):
+        existing_user = await User.filter(id=user_id).first()
+
+        if existing_user is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado"
+            )
+
+        existing_user.nome = user.nome
+        existing_user.email = user.email
+
+        # existing_user.password_hash = user.password - implementar o jwt depois
+
+        await existing_user.save()
+        return existing_user

9a8db841db579f3a5723b7423834b2b8c7047645
Lucas Oliveira <blesslucas89@gmail.com>
2026-08-05 12:37:23 -0500

    KAN-80 Implementar entidade/modelo de refresh token

--- src/services/AuthService.py
+++ src/services/AuthService.py
@@ -1,8 +1,12 @@
+from datetime import datetime, timedelta, timezone
+
 from fastapi import HTTPException, status
 
 from src.models.user import User
+from src.models.refresh_token import RefreshToken
 from src.schemas.auth import Login
-from src.core.security import verify_password, create_access_token
+from src.core.security import verify_password
+from src.services.jwt_service import create_access_token, create_refresh_token
 
 
 class AuthService:
@@ -19,6 +23,21 @@ class AuthService:
                 status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenciais inválidas"
             )
 
-        token = create_access_token({"sub": str(user.id), "email": user.email})
+        payload = {
+            "sub": str(user.id),
+            "email": user.email,
+        }
 
-        return {"access_token": token, "token_type": "bearer"}
+        access_token = create_access_token(payload)
+
+        refresh_token = create_refresh_token(payload)
+
+        expires_at = datetime.now(timezone.utc) + timedelta(days=7)
+
+        await RefreshToken.create(token=refresh_token, user=user, expires_at=expires_at)
+
+        return {
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+            "token_type": "bearer",
+        }

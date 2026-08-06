16ecdc3f4b13d23fa5f950aa0e2e1d8a4c13934f
Lucas Oliveira  Franca <126571905+lucasoliveira2023@users.noreply.github.com>
2026-08-06 11:11:15 -0500

    Revert "Kan 93 implementar logout"

--- src/routers/auth_router.py
+++ src/routers/auth_router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Depends
-from src.core.security import get_current_user
 from src.services.AuthService import AuthService
-from src.schemas.auth import Login, RefreshToken
+from src.schemas.auth import Login
 
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
@@ -10,15 +9,3 @@ router = APIRouter(prefix="/auth", tags=["Auth"])
 @router.post("/login")
 async def login(login: Login, service: AuthService = Depends()):
     return await service.login(login)
-
-
-@router.post("/refresh")
-async def refresh(data: RefreshToken, service: AuthService = Depends()):
-    return await service.refresh_token(data.refresh_token)
-
-
-@router.post("/logout")
-async def logout(
-    current_user=Depends(get_current_user), service: AuthService = Depends()
-):
-    return await service.logout(current_user)
